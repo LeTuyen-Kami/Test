e125da5bbd96e69f3402d6d770fed1e8a019c605
LeTuyen-Kami <82325497+LeTuyen-Kami@users.noreply.github.com>
2021-12-14 10:18:28 +0700

    complete

---
+++ LeTuyen.js
@@ -0,0 +1,16 @@
+//Write a js program to print out the console the integer numbers from 30 to 300 ,
+//but if that number that is multiples of 7 then print out 'abc',
+//if that number is multiples of 13 then print out 'xyz'.
+//For those numbers those are multiple of both 7 and 13 then print out 'a-z' .
+
+for (let i = 30; i <= 300; i++) {
+  if (i % 7 == 0 && i % 13 == 0) {
+    console.log("a-z");
+  } else if (i % 7 == 0) {
+    console.log("abc");
+  } else if (i % 13 == 0) {
+    console.log("xyz");
+  } else {
+    console.log(i);
+  }
+}
